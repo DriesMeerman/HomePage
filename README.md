f9d757112a546f2e2e7f6050be14ce74ce97b2a9
Dries Meerman <dries.meerman@pat4mation.com>
2018-09-04 00:40:24 +0200

    redid menu structure

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-#Homepage-v5
+#Homepage
 
-This version of my main homepage has had some major overhauls in not only look but in function as well. Favicons are long gone, and things are much more material-esque.
+This homepage has been forked from:
+ https://github.com/TacoAnon/TacoAnon.github.io  
+ 
+Because it was a nice looking simple to use template.
+Some changes have been made to code structure, to make it easier to update for me.
 
-##IMPORTANT
-Check out the script file in the JS directory. It's where you can setup your links, and change a few extra settings. Further explaination is provided in there.
-By default the links are non-functioning, as the menus were setup to for a video demo.
---
+++ js/menu.js
@@ -0,0 +1,38 @@
+var menu = [
+    {
+        svgIcon: svgCloudAlt,
+        noIconPadding: false,
+        name: "Services",
+        colorClass: "yellow",
+        links: [
+            {name: 'Youtube', url: "https://youtube.com", color: ""},
+            {name: 'Netflix', url: "https://netflix.com", color: ""},
+            {name: 'Gmail', url: "https://gmail.com", color: ""},
+            {name: 'Spotify', url: "https://open.spotify.com", color: ""},
+        ]
+    },
+    {
+        svgIcon: svgReddit,
+        noIconPadding: false,
+        name: "Reddit",
+        colorClass: "blue",
+        links: [
+            {name: 'Home', url: "https://old.reddit.com", color: ""},
+            {name: 'All', url: "https://old.reddit.com/r/all", color: ""},
+            {name: 'Programming', url: "https://old.reddit.com/r/programming", color: ""},
+            {name: 'Programmer Humor', url: "https://old.reddit.com/r/programmerhumor", color: ""},
+        ]
+    },
+    {
+        svgIcon: svgCode,
+        noIconPadding: false,
+        name: "Programming",
+        colorClass: "red",
+        links: [
+            {name: 'Github', url: "https://github.com", color: ""},
+            {name: 'Bitbucket', url: "https://bitbucket.com", color: ""},
+            {name: 'Stack Overflow', url: "https://stackoverflow.com", color: ""}
+        ]
+    },
+
+];
